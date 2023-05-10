a43178e79de0107b5a045e5d7933cbad25adde56
Martin Rasmussen <mra@trifork.com>
2023-05-10 20:44:54 +0200

    initia project

---
+++ MoodMeter/MoodMeter/MoodMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMeterApp.swift
+//  MoodMeter
+//
+//  Created by Martin Rasmussen on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoodMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
